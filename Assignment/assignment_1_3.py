0cf501a0ddd5aae6347abf5afb61742b0b715157
samPuriwat <puriwat.l@rmutsv.ac.th>
2022-08-24 16:09:47 +0700

    Initial commit

---
+++ Assignment/assignment_1_3.py
@@ -0,0 +1,16 @@
+
+
+def getPID():
+    pid = input('Enter your pid: ')  #str
+    lpid = [int(x) for x in pid]
+    return lpid # list
+
+def getFortune(var):
+    if var % 2 == 0:
+        print('Your fortune is good')
+    else:
+        print('Your fortune is very good')
+
+PID = getPID()
+print(f'The data from user: {sum(PID)}')
+print(f'The prediction result is: {getFortune(sum(PID))}')
